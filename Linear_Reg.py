d90f2c1dcd7fd0edda9926370878faa25929da8c
Nikhil <nparte777@gmail.com>
2020-03-31 04:20:53 +0530

    Simple Linear Regression Test Model

---
+++ Linear_Reg.py
@@ -0,0 +1,30 @@
+# Simple Linear Regression Model for Student Grades
+import tensorflow
+import pandas as pd
+import numpy as np
+import keras
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv",sep=";")
+data = data [["G1","G2","G3","studytime","failures","absences"]]
+predict = "G3"
+
+x= np.array(data.drop([predict], 1))
+y= np.array(data[predict])
+
+x_train, x_test, y_train, y_test =sklearn.model_selection.train_test_split(x,y,test_size = 0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train,y_train)
+acc=linear.score(x_test,y_test)
+print(acc)
+print("Co: \n",linear.coef_)
+print("Intercept: \n",linear.intercept_)
+
+predictions = linear.predict(x_test)
+# Printing Each Iteration of Data Being Predicted(Grade) : x_test[x] is Input Data  y_test[x] is the actual final grade
+for x in range(len(predictions)):
+    print(predictions[x],x_test[x],y_test[x])
