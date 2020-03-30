537780e926a719fbc756698c53fee74e508096ab
Nikhil <nparte777@gmail.com>
2020-03-31 04:17:03 +0530

    Prototype Perceptron from Scratch Status : W.I.P /Working with Random Function

---
+++ perceptron_01.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <random.h>
+
+class perceptron{
+  float weight[2];
+  perceptron(){
+    for(int i=0;i<seizeof(weight))
+    {
+      weight[i]=Use -1+2*((float)rand())/RAND_MAX
+    }
+  }
+}
