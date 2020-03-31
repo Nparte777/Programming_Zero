eb81416b9ce5a4f5b8054c0bc398a1c3b56e1623
Nikhil <nparte777@gmail.com>
2020-03-31 13:39:32 +0530

    Prototype Perceptron from Scratch Status : Finished(Warning Present Look into later)

--- perceptron_01.cpp
+++ perceptron_01.cpp
@@ -1,12 +1,44 @@
-#include <bits/stdc++.h>
-#include <random.h>
-
+#include <iostream>
+#include <stdio.h>
+#include <random>
+using namespace std;
+int sign(int n){
+  if (n>0){return 1;}
+  else{return -1;}
+}
 class perceptron{
+  int sum=0;
+  //randomize();
   float weight[2];
-  perceptron(){
-    for(int i=0;i<seizeof(weight))
+public:
+    void weight_cal(){
+    int HIGH = -1;
+    int LOW  =  1;
+    int n = sizeof(weight);
+    int i;
+    for(i=0;i<n;i++)
     {
-      weight[i]=Use -1+2*((float)rand())/RAND_MAX
+      weight[i]=rand() % (HIGH - LOW + 1) + LOW;
     }
   }
+
+  int guess(float inputs[],int size)
+  {
+    for (int i=0;i<sizeof(weight);i++)
+    {
+      sum += inputs[i]*weight[i];
+    }
+
+  int output = sign (sum);
+  return output;
+ }
+};
+
+int main(){
+  perceptron p;
+  float inputs[]={-1,0.5};
+  int n=sizeof(inputs);
+  int guess = p.guess(inputs,n);
+  cout<<guess;
+  return 0;
 }
